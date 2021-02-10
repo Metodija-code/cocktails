3334242e207b63215d262dbb0223ecb0befcb3ed
Metodija-code <metoidja_mitovski@hotmail.com>
2021-02-10 22:28:25 +0100

    main commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <section className="section about-section">
+        <h1 className="section-title">about us</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas
+          dicta odit ea explicabo commodi et est, tempora saepe provident
+          laborum quaerat quas consequuntur mollitia minima eos nulla deleniti
+          magnam vel?
+        </p>
+      </section>
+    </>
+  );
+};
+
+export default About;
